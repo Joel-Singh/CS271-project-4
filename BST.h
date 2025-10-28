15880da1cf050d5c750b75d60563dea532cafc8b
Joel Singh <joelsingh788@gmail.com>
2025-10-28 16:24:17 -0400

    Make helper methods privatee

--- BST.h
+++ BST.h
@@ -29,14 +29,14 @@ public:
   K min_key();
   K successor(K k);
   string to_string() const;
-  string in_order(Node<D, K>* x);
   string in_order();
   void trim(K low, K high);
-  void trim(K low, K high, Node<D, K>* current);
 private:
   void transplant(Node<D, K>* u, Node<D, K>* v);
   Node<D, K>* min(Node<D, K>* node);
   Node<D, K>* remove_node(Node<D, K>* node);
+  string in_order(Node<D, K>* x);
+  void trim(K low, K high, Node<D, K>* current);
   
   Node<D, K>* root;
 };
